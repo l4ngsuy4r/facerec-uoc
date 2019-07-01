5e8f3cf9e2b93aad47b80f6a332679aa6ec304b4
Mario Gascón Viúdez <mario.gascon@es.gfi.world>
2019-07-01 16:33:15 +0200

    add basic positioning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatButtonModule } from '@angular/material'
 
 @NgModule({
   declarations: [AppComponent, SignUpComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
