16dae1419ce45dd9c8b2a4590ae96238218cccf0
Mario Gascón Viúdez <mario.gascon@es.gfi.world>
2019-07-03 12:20:57 +0200

    before implementing stepper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-
 import { AppComponent } from './app.component'
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpComponent } from './sign-up/sign-up.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatButtonModule } from '@angular/material'
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule, MatDividerModule,
+  MatIconModule, MatListModule, MatToolbarModule,
+  MatTooltipModule
+} from '@angular/material'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [AppComponent, SignUpComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatTooltipModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    MatToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -3,7 +3,9 @@ import {
   Component,
   ElementRef,
   OnDestroy,
-  ViewChild
+  QueryList,
+  ViewChild,
+  ViewChildren
 } from '@angular/core'
 
 @Component({
@@ -12,24 +14,43 @@ import {
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements AfterViewInit, OnDestroy {
-  @ViewChild('player') private playerRef: ElementRef<HTMLVideoElement>
-  @ViewChild('canvas') private canvasRef: ElementRef<HTMLCanvasElement>
+  @ViewChild('player')
+  private playerRef: ElementRef<HTMLVideoElement>
+  @ViewChild('canvas')
+  private canvasRef: ElementRef<HTMLCanvasElement>
+
+  @ViewChildren(HTMLImageElement)
+  private images: QueryList<ElementRef<HTMLImageElement>>
 
   private player: HTMLVideoElement
   private canvas: HTMLCanvasElement
   private context: CanvasRenderingContext2D
 
-  ngAfterViewInit (): void {
+  private readonly numberOfPics = 7
+
+  pictures: string[] = []
+  capturing = true
+  conditions = false
+
+  ngAfterViewInit () {
     this.player = this.playerRef.nativeElement
     this.canvas = this.canvasRef.nativeElement
     this.context = this.canvas.getContext('2d')
+
+    // start camera stream
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(stream => (this.player.srcObject = stream))
   }
 
   ngOnDestroy () {
-    this.stopStream()
+    // stop camera stream
+    (this.player.srcObject as MediaStream)
+      .getVideoTracks()
+      .forEach(track => track.stop())
   }
 
-  captureImage () {
+  capture () {
     this.context.drawImage(
       this.player,
       0,
@@ -37,17 +58,23 @@ export class SignUpComponent implements AfterViewInit, OnDestroy {
       this.canvas.width,
       this.canvas.height
     )
+    this.pictures.push(this.canvas.toDataURL('image/png'))
+    if (this.pictures.length === this.numberOfPics) {
+      this.capturing = false
+      this.submit()
+    }
   }
 
-  startStream () {
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then(stream => (this.player.srcObject = stream))
+  retry () {
+    this.pictures = []
+    this.capturing = true
   }
 
-  stopStream () {
-    (this.player.srcObject as MediaStream)
-      .getVideoTracks()
-      .forEach(track => track.stop())
+  private submit () {
+    console.log({
+      picture1: this.pictures[0],
+      picture2: this.pictures[1],
+      picture3: this.pictures[2]
+    })
   }
 }
